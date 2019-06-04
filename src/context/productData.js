8e453dac34d9f71e025372d5280b8a6b79000fa1
pranav134 <pranav.134.pg@gmail.com>
2019-06-05 00:59:44 +0530

    MY First Commit

---
+++ src/context/productData.js
@@ -0,0 +1,28 @@
+export const items = [
+  {
+    sys: { id: 0},
+    fields: {
+      title: "Urdu Calligraphy done an a T shirt",
+      price: 20.00,
+      company: "Kitabat",
+      description:
+        "Under our Kitabat initiative, we have expanded by coming different products with Urdu Calligraphy. The T-shirt is a union of artistic Urdu Calligraphy and Smooth Cottom apparel wear. On the front we have an Urdu Drawing in the shape of a key.",
+      featured: true,
+      image: { fields: { file: { url: "img/kitabat_tshirt.jpg" } } },
+      freeShipping: true
+    }
+  },
+  {
+    sys: { id: 1},
+    fields: {
+      title: "Aesthetic frame in Urdu calligraphy",
+      price: 21.99,
+      company: "Kitabat",
+      description:
+        "One of the most premium calligraphy artworks designed by the renowned katibs of Old Delhi themselves. ",
+      featured: true,
+      image: { fields: { file: { url: "img/kitabat_frame.jpg" } } },
+      freeShipping: true
+    }
+  }
+];
