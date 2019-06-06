c9623c60d1b7ce6904afee59356dbc1db5303675
pranav134 <pranav.134.pg@gmail.com>
2019-06-06 23:19:12 +0530

    no message

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from "react";
 import { FaBars, FaCartPlus } from "react-icons/fa";
 import styled from "styled-components";
 import { ProductConsumer } from "../context";
-import logo from "../images/logo.svg";
+{/*import logo from "../images/logo.svg";*/}
 export default function Navbar() {
   return (
     <ProductConsumer>
